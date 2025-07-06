b53b12286c1af1775ca5b3ca64754b28d3e6b83c
ramon-dacioli <ramon_muz@hotmail.com>
2025-07-06 11:55:51 -0300

    Seleção dos textos 1

---
+++ 10-exemplo.py
@@ -0,0 +1,25 @@
+import pytesseract
+import numpy as np
+import cv2
+from pytesseract import Output
+
+img = cv2.imread('imagens/teste_manuscrito_01.jpg')
+rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+cv2.imshow("Imagem", rgb)
+#cv2.waitKey(0)
+
+config_tesseract = '--tessdata-dir tessdata'
+resultado = pytesseract.image_to_data(rgb, config=config_tesseract, lang='por', output_type=Output.DICT)
+print(resultado)
+
+min_conf = 40
+
+def caixa_texto(resultado, img, cor = (255,100,0)):
+    x = resultado['left'][i]
+    y = resultado['top'][i]
+    w = resultado['width'][i]
+    h = resultado['height'][i]
+
+    cv2.rectangle(img, (x, y), (x, y), cor, 2)
+
+    return x, y, img
