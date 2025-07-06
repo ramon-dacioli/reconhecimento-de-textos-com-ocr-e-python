5a89e19822e7241d0705cac78c9e507872edabd5
ramon-dacioli <ramon_muz@hotmail.com>
2025-07-06 12:09:32 -0300

    Seleção dos textos 2

--- 10-exemplo.py
+++ 10-exemplo.py
@@ -20,6 +20,17 @@ def caixa_texto(resultado, img, cor = (255,100,0)):
     w = resultado['width'][i]
     h = resultado['height'][i]
 
-    cv2.rectangle(img, (x, y), (x, y), cor, 2)
+    cv2.rectangle(img, (x, y), (x + w, y + h), cor, 2)
 
     return x, y, img
+
+img_copia = rgb.copy()
+for i in range(0, len(resultado['text'])):
+    confianca = int(resultado['conf'][i])
+    if confianca > min_conf:
+        x, y, img = caixa_texto(resultado, img_copia)
+        print(x, y)
+        texto = resultado['text'][i]
+        cv2.putText(img_copia, texto, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.1, (0,0,255))
+cv2.imshow("Imagem", img_copia)
+cv2.waitKey(0)
